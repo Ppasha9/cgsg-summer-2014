4da91df2f20e255e2d5c46ec4d99340264bae567
kbsx32 <BlackSmithX32@gmail.com>
2014-06-13 00:09:16 +0400

    Camera automated

--- T06ANIM/UNITS/SAMPCUBE.C
+++ T06ANIM/UNITS/SAMPCUBE.C
@@ -32,18 +32,57 @@ typedef struct tagrk2UNIT_CUBE
  */
 static VOID UnitCubeInit( rk2UNIT_CUBE *Unit, rk2ANIM *Ani )
 {
+  INT i;
+
   Unit->Points[0].X = Unit->PosX;                   Unit->Points[0].Y = Unit->PosY;                   Unit->Points[0].Z = Unit->PosZ;
-  Unit->Points[1].X = Unit->PosX + Unit->FacetLen;  Unit->Points[1].Y = Unit->PosY + Unit->FacetLen;  Unit->Points[1].Z = Unit->PosZ;
-  /*
-  Unit->Points[1].X = Unit->PosX + Unit->FacetLen;  Unit->Points[1].Y = Unit->PosY + Unit->FacetLen;  Unit->Points[1].Z = Unit->PosZ;
-  Unit->Points[1].X = Unit->PosX + Unit->FacetLen;  Unit->Points[1].Y = Unit->PosY + Unit->FacetLen;  Unit->Points[1].Z = Unit->PosZ;
-  Unit->Points[1].X = Unit->PosX + Unit->FacetLen;  Unit->Points[1].Y = Unit->PosY + Unit->FacetLen;  Unit->Points[1].Z = Unit->PosZ;
-  Unit->Points[1].X = Unit->PosX + Unit->FacetLen;  Unit->Points[1].Y = Unit->PosY + Unit->FacetLen;  Unit->Points[1].Z = Unit->PosZ;
-  Unit->Points[1].X = Unit->PosX + Unit->FacetLen;  Unit->Points[1].Y = Unit->PosY + Unit->FacetLen;  Unit->Points[1].Z = Unit->PosZ;
-  Unit->Points[1].X = Unit->PosX + Unit->FacetLen;  Unit->Points[1].Y = Unit->PosY + Unit->FacetLen;  Unit->Points[1].Z = Unit->PosZ;
-  Unit->Points[1].X = Unit->PosX + Unit->FacetLen;  Unit->Points[1].Y = Unit->PosY + Unit->FacetLen;  Unit->Points[1].Z = Unit->PosZ;
-  */
+  for (i = 1; i < 9; i++)
+    Unit->Points[i].X = rand() % 500 - 1000,  Unit->Points[i].Y = rand() % 500 - 1000, Unit->Points[i].Z = rand() % 500 - 1000;
 } /* End of 'RK2_UnitCubeInit' function */
+
+
+/* Unit cube getting coords function.
+* ARGUMENTS:
+ *   - Self pointer:
+ *       rk2UNIT *Unit;
+ *   - Animation context pointer:
+ *       rk2ANIM *Ani;
+ *   - Last point position:
+ *       rk2VEC SrcPoint;
+ *   - Time:
+ *       DBL Time;
+ * RETURNS: 
+ *   (rk2VEC) Coords of point.
+ */
+static rk2VEC UnitCubeGetCoordByTime(rk2UNIT_CUBE *Unit, rk2ANIM *Ani, rk2VEC SrcPoint, DBL Time )
+{
+  rk2VEC VecSaved = SrcPoint;
+  static INT MinDist = 30, MaxDist = 300;
+
+  SrcPoint.X += cos(Time * 3) * ((INT)Time % 20);
+  if (SrcPoint.X < -MaxDist)
+    SrcPoint.X = -MaxDist;
+  if (SrcPoint.X > MaxDist)
+    SrcPoint.X = MaxDist;
+
+  SrcPoint.Y += sin(Time) * ((INT)Time % 10);
+  if (SrcPoint.Y < -MaxDist)
+    SrcPoint.Y = -MaxDist;
+  if (SrcPoint.Y > MaxDist)
+    SrcPoint.Y = MaxDist;
+  
+  SrcPoint.Z += sin(Time * 2) * ((INT)Time % 30);
+  if (SrcPoint.Z< -MaxDist)
+    SrcPoint.Z = -MaxDist;
+  if (SrcPoint.Z > MaxDist)
+    SrcPoint.Z = MaxDist;
+
+  if (SrcPoint.X > -MinDist && SrcPoint.X < 0 &&
+      SrcPoint.Y > -MinDist && SrcPoint.Y < 0 &&
+      SrcPoint.Z > -MinDist && SrcPoint.Z < 0)
+    return VecSaved;
+
+  return SrcPoint;
+} /* End of 'UnitCubeGetCoordByTime' function */
 
 /* Unit cube response function.
  * ARGUMENTS:
@@ -55,13 +94,7 @@ static VOID UnitCubeInit( rk2UNIT_CUBE *Unit, rk2ANIM *Ani )
  */
 static VOID UnitCubeResponse( rk2UNIT_CUBE *Unit, rk2ANIM *Ani )
 {
-  INT i;
-  for (i = 0; i < 9; i++)
-  {
-    Unit->Points[i].X += cos(Ani->Time);
-    Unit->Points[i].Y += cos(Ani->Time);
-    Unit->Points[i].Z += cos(Ani->Time) / 100;
-  }
+  Unit->Points[0] = UnitCubeGetCoordByTime(Unit, Ani, Unit->Points[0], Ani->Time);
 } /* End of 'RK2_UnitCubeResponse' function */
 
 /* Unit cube render function.
@@ -75,19 +108,23 @@ static VOID UnitCubeResponse( rk2UNIT_CUBE *Unit, rk2ANIM *Ani )
 static VOID UnitCubeRender( rk2UNIT_CUBE *Unit, rk2ANIM *Ani )
 {
   rk2VEC NPnt;
-  INT i, ls = 2;
+  INT ls = 2;
   DBL j;
   CHAR Str[MAX_PATH];
 
-  for (i = 0; i < 9; i++)
-  {
-    NPnt = RK2_RndWorldToScreen(Unit->Points[i]);
-    Ellipse(Ani->hDC, NPnt.X - 5 / NPnt.Z, NPnt.Y - 5 / NPnt.Z, NPnt.X + 5 / NPnt.Z, NPnt.Y + 5 / NPnt.Z);
-  }
-  
+  RK2_RndCameraSet(Unit->Points[0], VecSet(0, 0, 0), VecSet(0, 1, 0));
+  /* UnitCubeGetCoordByTime(Unit, Ani, Unit->Points[0], Ani->Time + 0.2); */
+
   /* Axes */
   SelectObject(Ani->hDC, GetStockObject(DC_BRUSH));
   SelectObject(Ani->hDC, GetStockObject(NULL_PEN));
+
+  RK2_RndMatrWorld = MatrDefault();
+  RK2_RndBuildMatrix();
+
+  NPnt = RK2_RndWorldToScreen(Unit->Points[0]);
+  Ellipse(Ani->hDC, NPnt.X - 5 / NPnt.Z, NPnt.Y - 5 / NPnt.Z, NPnt.X + 5 / NPnt.Z, NPnt.Y + 5 / NPnt.Z);
+
 
   for (j = -50; j <= 50; j += 0.5)
   {
@@ -130,7 +167,7 @@ rk2UNIT *RK2_UnitCubeCreate( VOID )
   Unit->Init = (VOID *)UnitCubeInit;
   Unit->Response = (VOID *)UnitCubeResponse;
 
-  Unit->PosX = Unit->PosY = Unit->PosZ = 0;
+  Unit->PosX = Unit->PosY = Unit->PosZ = 70;
   return (rk2UNIT *)Unit;
 } /* End of 'RK2_UnitCubeCreate' function */
 
